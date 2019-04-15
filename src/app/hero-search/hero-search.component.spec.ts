5bec0fe4869eb5eada9c8e277d64165149933745
alisaduncan <7586237+alisaduncan@users.noreply.github.com>
2019-04-15 13:19:47 -0500

    fix unit and e2e tests

--- src/app/hero-search/hero-search.component.spec.ts
+++ src/app/hero-search/hero-search.component.spec.ts
@@ -1,14 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeroSearchComponent } from './hero-search.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { HeroService } from '../hero.service';
 
 describe('HeroSearchComponent', () => {
   let component: HeroSearchComponent;
   let fixture: ComponentFixture<HeroSearchComponent>;
 
+  const mockHerosService = {
+    searchHeroes: (search) => {}
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroSearchComponent ]
+      declarations: [ HeroSearchComponent ],
+      providers: [ { provide: HeroService, useValue: mockHerosService }],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
